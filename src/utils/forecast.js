481bd2ec6162f4e17dd9517c68f4e03a2aac1dfa
Blake <blakedippolito@gmail.com>
2022-08-20 16:17:45 -0700

    Added forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, (body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain'))
+   
+            callback(undefined, (body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain and the wind is blowing at ' + body.current.wind_speed + ' mph'))
         }
 
     })
